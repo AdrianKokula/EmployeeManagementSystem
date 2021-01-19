04e061af46e50dfc716149b5a583e488dd6af6f4
Adrián Kokuľa <kokula.adrian45@gmail.com>
2021-01-19 22:53:35 +0100

    Added application styles, changed design in some windows/pages

--- EmployeeManagementSystem/MainWindow.xaml.cs
+++ EmployeeManagementSystem/MainWindow.xaml.cs
@@ -44,6 +44,19 @@ namespace EmployeeManagementSystem {
 
 		#region "handlers"
 
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e) {
+
+			if (e.ChangedButton == MouseButton.Left) {
+				this.Cursor = Cursors.SizeAll;
+				this.DragMove();
+			}
+
+		}
+
+		private void Window_MouseUp(object sender, MouseButtonEventArgs e) {
+			this.Cursor = Cursors.Arrow;
+		}
+
 		private void BtnClose_Click(object sender, RoutedEventArgs e) {
 			Close();
 		}
@@ -79,5 +92,6 @@ namespace EmployeeManagementSystem {
 
 
 		#endregion
+
 	}
 }
